14c1a9e0425bd9ec6fa6c8b4f5d788342e0047c5
Olena Iliushyn <lenailyshun@gmail.com>
2026-06-01 17:43:42 +0300

    Intro

---
+++ 01_lesson.py
@@ -0,0 +1,28 @@
+print("Hello world")#Ctrl+F5
+
+print("one","two","three")
+
+print("I")
+print("Love")
+print("Python!")
+print("I\n\tLove\n\t\t\"Python\"!")
+#escape sequnces"
+#\n - перехід на новий рядочок
+#\" - "Python"
+#\t - ставить табуляцію, або 8 пробілів
+#Shift+Alt+ arrow up or arrow down - робимо дублікат рядочка
+print('Hello world')
+
+print('P','y','t','h','o','n', sep="")
+print('P','y','t','h','o','n', sep="*")
+print('P','y','t','h','o','n', sep="_")
+print('P','y','t','h','o','n', sep="\n")
+print('Python')
+
+print("one","two","three",'five','six','seven','eight',sep='' )
+print("one","two","three",'five','six','seven','eight',sep='\n' )
+print("one","two","three",'five','six','seven','eight',sep='__' )
+print()
+print("\\Python\\", end=" ")
+
+print(" - is my first project!")
