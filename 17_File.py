a896beb584f29e2074db5b037d30816e800f6cd3
Olena Iliushyn <lenailyshun@gmail.com>
2026-08-17 17:08:23 +0300

    Dictionary

--- 17_File.py
+++ 17_File.py
@@ -1,91 +1,113 @@
-# name.txt
-# name.html
-# name.py
-# name.cpp
-# name.cs
-# name.png
-# name.css
-# name.exe
-# name.js
+# # name.txt
+# # name.html
+# # name.py
+# # name.cpp
+# # name.cs
+# # name.png
+# # name.css
+# # name.exe
+# # name.js
 
-# open file
-# read file
-# write file
-# close file
+# # open file
+# # read file
+# # write file
+# # close file
 
-url = r'C:\Users\helen\Desktop\PD_621_Python\my_file.txt'
-#file = open('my_file.txt')
-# file = open(url)
+# url = r'C:\Users\helen\Desktop\PD_621_Python\my_file.txt'
+# #file = open('my_file.txt')
+# # file = open(url)
 
-# #print(file.read())
-# #print(file.readline().strip())
-# #print(file.readlines())
-# print(file.read(10))
-# file.close()
-with open(url) as file:
-    print(file.read())
-    #file.close()
+# # #print(file.read())
+# # #print(file.readline().strip())
+# # #print(file.readlines())
+# # print(file.read(10))
+# # file.close()
+# with open(url) as file:
+#     print(file.read())
+#     #file.close()
 
-with open('write_file_rewrite.txt', 'w') as file:
-    file.write("Hello world\n")
+# with open('write_file_rewrite.txt', 'w') as file:
+#     file.write("Hello world\n")
 
-with open('write_file_append.txt', 'a') as file:
-    file.write("Hello world\n")
+# with open('write_file_append.txt', 'a') as file:
+#     file.write("Hello world\n")
 
-lines = [
-    'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-    'Lorem Ipsum has been the industrys standard dummy text ever since 1966, ',
-    'when designers at Letraset and James Mosley, the librarian at St Bride ',
-    'Printing Library in London, took a 1914 Cicero translation and scrambled '
-]
-url_2 = "./write_lines.txt"
-# counter = 1
+# lines = [
+#     'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+#     'Lorem Ipsum has been the industrys standard dummy text ever since 1966, ',
+#     'when designers at Letraset and James Mosley, the librarian at St Bride ',
+#     'Printing Library in London, took a 1914 Cicero translation and scrambled '
+# ]
+# url_2 = "./write_lines.txt"
+# # counter = 1
+# # with open(url_2, 'w' ) as file:
+# #     #file.write(lines) error
+# #     for line in lines:
+# #         file.write(f"Line number [{counter}] : {line}\n")
+# #         counter+=1
+
 # with open(url_2, 'w' ) as file:
-#     #file.write(lines) error
-#     for line in lines:
-#         file.write(f"Line number [{counter}] : {line}\n")
-#         counter+=1
-
-with open(url_2, 'w' ) as file:
-    file.writelines(lines)
+#     file.writelines(lines)
 
 
 
-def readFile(filename):
-    with open(filename, 'r' ) as file:
-        #return file.read()
-        return file.readlines()
+# def readFile(filename):
+#     with open(filename, 'r' ) as file:
+#         #return file.read()
+#         return file.readlines()
 
-def appFileToEnd(filename, info):
-    with open(filename,'a') as file:
-        file.write(info)
+# def appFileToEnd(filename, info):
+#     with open(filename,'a') as file:
+#         file.write(info)
 
-def appFileToEndList(filename, info_list):
-    with open(filename,'a') as file:
-        for line in info_list[::-1]:
-            file.write(f"{line.strip()}\n")
-url_write = "file_write_func.txt"
-url_read = "file_write_func.txt"
+# def appFileToEndList(filename, info_list):
+#     with open(filename,'a') as file:
+#         for line in info_list[::-1]:
+#             file.write(f"{line.strip()}\n")
+# url_write = "file_write_func.txt"
+# url_read = "file_write_func.txt"
 
-text= readFile('my_file.txt')
-print(text)
-print(text[::-1])
+# text= readFile('my_file.txt')
+# print(text)
+# print(text[::-1])
 
-#appFileToEnd(url_write, text)
-appFileToEndList(url_write, text)
+# #appFileToEnd(url_write, text)
+# appFileToEndList(url_write, text)
 
 
-def testModeFile(filename):
-    with open(filename, 'a+') as file:
-        print(file.readable())
-        print(file.writable())
+# def testModeFile(filename):
+#     with open(filename, 'a+') as file:
+#         print(file.readable())
+#         print(file.writable())
 
-testModeFile('my_file.txt')
+# testModeFile('my_file.txt')
 
-with open("write_numbers.txt",'w') as file:
-    file.write(str(55))
+# with open("write_numbers.txt",'w') as file:
+#     file.write(str(55))
 
-print('a')
-print(chr(65))
-print(chr(65+1))
-print(chr(65+2))
+# print('a')
+# print(chr(65))
+# print(chr(65+1))
+# print(chr(65+2))
+
+
+#Напишіть програму, яка копіює вміст файлу 
+# data.txt у новий файл backup.txt.
+# with open('data.txt','w') as file:
+#     file.write("Hello world!\nHello world!\nHello world!")
+
+# def readFile(filename):
+#     with open(filename, 'r') as file:
+#         return file.read()
+
+
+# def copyFile(filename, text):
+#     with open(filename, 'w') as file:
+#         file.write(text)
+# copyText = readFile('data.txt')
+# copyFile('backup.txt',copyText)
+
+
+with open('test.txt', 'w+') as file:
+    file.write("lzxflkjkdls")
+    print(file.readline())
---
+++ 18_Dictionary.py
@@ -0,0 +1,130 @@
+import json
+
+login = "user1"
+password = "123456"
+email = "user1@gmal.com"
+score = 0
+hp = 100
+
+
+login1 = "user1"
+password1 = "123456"
+email1 = "user1@gmal.com"
+score1 = 0
+hp1 = 100
+
+user1 = [ "user1","123456", "user1@gmal.com", 0, 100]
+user2 = [ "user1","123456", "user1@gmal.com", 0, 100]
+user3 = [ "user1","123456", "user1@gmal.com", 0, 100]
+user4 = [ "user1","123456", "user1@gmal.com", 0, 100]
+
+# dictionary {} -->  key:value
+
+student = {
+    #key(only str): value (any type)
+    'name':'Oleg',
+    'surname':'Ivanchuk',
+    'age':17,
+    'study place': "IT Step",
+    'rating' : 11.8,
+    'group': 'PD621',
+    'course':'programming',
+    'birthdate':'01.06.2009'
+}
+print(student)
+print(student["name"])
+print(student['surname'])
+print(student['study place'])
+print(f"Fullname : {student["name"]} {student["surname"]}.\nGroup {student['group']}. RAting {student['rating']}")
+
+for key in student.keys():
+    print(f"{key} - {student[key]}")
+
+print()
+
+for value in student.values():
+    print(f"\t{value}")
+
+print()
+for key, value in student.items():
+    print(f"{key} - {value}")
+
+print(student.keys())
+print(student.values())
+print(student.items())
+
+print(student)
+del student['birthdate']
+print(student)
+student.popitem()
+print(student)
+student.pop('age')
+print(student)
+
+student['email'] = 'superpuperuser@gmail.com'
+student['emaile'] = 'superpuperuser@gmail.com'
+student['homeaddress'] = 'Rivne, Soborna 285'
+print(student)
+
+
+students_list = [
+    {'name1':"Stas",'surname':"Ivanchuk",'rating':11.5, 'birthdate':"01.06.2009"},
+    {'name1':"Olga",'surname':"Popchuk",'rating':7.5, 'birthdate':"01.12.2009", "marks":[7,6,5,8,10,11]},
+    {'name1':"Mukola",'surname':"Oliunuk",'rating':9.8, 'birthdate':"01.10.2009"},
+    {'name1':"Ira",'surname':"Petruk",'rating':11.0, 'birthdate':"01.04.2009"}
+]
+print(students_list)
+print(students_list[1]["name1"])
+print(students_list[1]["rating"])
+print(students_list[1]["marks"])
+print(students_list[1]["marks"][0])
+
+print("MArks Olga : ")
+for m in students_list[1]["marks"]:
+    print(m, end= " ")
+print()
+
+print(student)
+# name_key = input("Enter key to change value : ")
+# for item in students_list:
+#     print(item.keys())
+#     if name_key in item.keys():
+#         print(f"{name_key} is in dictionary ")
+#     else:
+#         print("key not found")
+student = {
+    #key(only str): value (any type)
+    'name':'Oleg',
+    'surname':'Ivanchuk',
+    'age':17,
+    'study place': "IT Step",
+    'rating' : 11.8,
+    'group': 'PD621',
+    'course':'programming',
+    'birthdate':'01.06.2009'
+}
+with open("student_save.json",'w') as file:
+    student_serialize = json.dumps(student)
+    file.write(student_serialize)
+
+print("Read student from file")
+with open("student_save.json",'r') as file:
+    info = file.read()
+    #print(file.read()['name'])
+    new_student = json.loads(info)
+    print(new_student)
+
+
+
+
+
+# print(type(student))
+# print(student)
+
+# student_serialize = json.dumps(student)
+# print(type(student_serialize))
+# print(student_serialize)
+
+# new_student = json.loads(student_serialize)
+# print(type(new_student))
+# print(new_student['name'])
